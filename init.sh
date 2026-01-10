2a371b1761f692f3ec7b720c7939396c3f71462c
projit32 <projitchandra32@gmail.com>
2026-01-10 15:49:12 +0530

    added init sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+apt-get -y update
+apt-get -y upgrade
+apt-get -y pwdinstall bluetooth bluez
+WORK_DIR=$(pwd)
+echo $WORK_DIR
+mkdir -p $WORK_DIR/obd
+cd $WORK_DIR/obd
+pwd
+python -m venv obd-env
+source obd-env/bin/activate
+apt install libbluetooth-dev
+
