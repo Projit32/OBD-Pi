6898d1a0f64116a74f4b34a6a796a00e2aa69027
projit32 <projitchandra32@gmail.com>
2026-01-24 11:27:17 +0530

    minor modifications

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ bluetoothctl -- power on
 source $WORK_DIR/obd-env/bin/activate
 rfcomm connect /dev/rfcomm0 $OBD_MAC_ID $OBD_CANNEL_ID &
 sleep 2
-python $WORK_DIR/OBD-Pi/app.py
+python $WORK_DIR/app.py
