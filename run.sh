14c28d57067d284b99c109c1a631a6d1504f274a
projit32 <projitchandra32@gmail.com>
2026-01-24 10:03:17 +0530

    Updated run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 rfkill unblock bluetooth
 bluetoothctl -- power on
-source $WORK_DIR/obd/obd-env/bin/activate
+source $WORK_DIR/obd-env/bin/activate
 rfcomm connect /dev/rfcomm0 $OBD_MAC_ID $OBD_CANNEL_ID &
 sleep 2
 python $WORK_DIR/OBD-Pi/app.py
